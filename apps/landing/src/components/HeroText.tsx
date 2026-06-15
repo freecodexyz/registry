0a5b4487111b7137bd13896e1d61a89cd875820f
paoloanzn <paolo@gladium.ai>
2026-06-15 16:51:32 +0200

    (chore) adjust copy

--- apps/landing/src/components/HeroText.tsx
+++ apps/landing/src/components/HeroText.tsx
@@ -7,7 +7,7 @@ export function HeroText() {
     <div className="hero-text">
       <h1 className="hero-text__heading">The Decentralized Marketplace Of Open Source Software</h1>
       <p className="hero-text__subheading">
-        An open decentralized market that turns your open source code repo into a tokenized cashflow generating asset
+        Open decentralized market that turns open source code into a tokenized cashflow generating asset
       </p>
       <div className="hero-text__actions" aria-label="Hero actions">
         <ButtonLink className="hero-text__button" href={appUrl}>Enter App</ButtonLink>
